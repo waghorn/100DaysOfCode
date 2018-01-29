9e6b87cc057d0d2f18ecbb768263c060192f3666
Chris W <waghorn.chris@hotmail.co.uk>
2018-01-29 09:34:19 +0000

    100Days Chapter two added new questions and created question class

---
+++ GeoQuiz/app/src/main/java/com/christopherwaghorn/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.christopherwaghorn.geoquiz;
+
+/**
+ * Created by christopherwaghorn on 29/01/2018.
+ *
+ * Class for representing questions
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}

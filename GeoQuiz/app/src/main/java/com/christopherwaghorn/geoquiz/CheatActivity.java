8210c102c116151e58b817a4255da7ca17ec960f
Chris W <waghorn.chris@hotmail.co.uk>
2018-02-02 18:33:46 +0000

    chapter five complete

---
+++ GeoQuiz/app/src/main/java/com/christopherwaghorn/geoquiz/CheatActivity.java
@@ -0,0 +1,57 @@
+package com.christopherwaghorn.geoquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.christopherwaghorn.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.christopherwaghorn.geoquiz.answer_shown";
+    private boolean mAnswerIsTrue;
+
+    private Button mShowAnswer;
+    private TextView mAnswerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+
+        mShowAnswer = findViewById(R.id.show_answer_button);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+}

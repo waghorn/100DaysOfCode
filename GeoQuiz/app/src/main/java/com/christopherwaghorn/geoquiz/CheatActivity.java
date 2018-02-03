f9a7d69179758f65cc6b20305abc2770c7f02302
Chris W <waghorn.chris@hotmail.co.uk>
2018-02-03 19:29:58 +0000

    Completed chchallenages for chapter five

--- GeoQuiz/app/src/main/java/com/christopherwaghorn/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/christopherwaghorn/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,8 +13,11 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.christopherwaghorn.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.christopherwaghorn.geoquiz.answer_shown";
-    private boolean mAnswerIsTrue;
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_SHOWN = "shown";
 
+    private boolean mAnswerIsTrue;
+    private boolean mAnswerIsShown;
     private Button mShowAnswer;
     private TextView mAnswerTextView;
 
@@ -29,6 +33,7 @@ public class CheatActivity extends AppCompatActivity {
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mAnswerIsShown = true;
                 if (mAnswerIsTrue) {
                     mAnswerTextView.setText(R.string.true_button);
                 } else {
@@ -37,6 +42,11 @@ public class CheatActivity extends AppCompatActivity {
                 setAnswerShownResult(true);
             }
         });
+
+        if (savedInstanceState != null) {
+            mAnswerIsShown = savedInstanceState.getBoolean(KEY_SHOWN, false);
+            setAnswerShownResult(mAnswerIsShown);
+        }
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
@@ -53,5 +63,12 @@ public class CheatActivity extends AppCompatActivity {
 
     public static boolean wasAnswerShown(Intent result) {
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSavedInstanceState() called");
+        savedInstanceState.putBoolean(KEY_SHOWN, mAnswerIsShown);
     }
 }
